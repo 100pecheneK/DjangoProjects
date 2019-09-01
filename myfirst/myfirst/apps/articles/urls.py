d6dca187ba866a0bc2394155eccb30c5a26760e7
100pecheneK <47976264+100pecheneK@users.noreply.github.com>
2019-09-01 15:56:15 +0500

    1
    2

--- myfirst/myfirst/apps/articles/urls.py
+++ myfirst/myfirst/apps/articles/urls.py
@@ -5,8 +5,11 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/leave_comment/', views.leave_comment, name='leave_comment')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,5 +1,28 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Article
 
 
 def index(request):
-    return render(request, 'articles/list.html')
+    latest_articles_list = Article.objects.order_by('-pub_date')[:10]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
+
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена")
+    latest_comments_list = a.comment_set.order_by('-id')
+    return render(request, 'articles/single.html', {'article': a, 'latest_comments_list': latest_comments_list})
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена")
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['comment'])
+    return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
